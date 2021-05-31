de44820993e589ba3cf3aa0bd6d72585052f37bb
Tamás Kálcza <tamas.kalcza@digitalasset.com>
2021-05-31 11:51:41 +0200

    Cleanups.

--- example-acord/app/src/main/scala/com/digitalasset/app/Commands.scala
+++ example-acord/app/src/main/scala/com/digitalasset/app/Commands.scala
@@ -13,16 +13,14 @@ import org.slf4j.{Logger, LoggerFactory}
 import role.operator.OperatorRole
 
 import java.net.URL
-import java.time.Instant
 
 object Commands {
   private val config = ConfigFactory.load()
   private val client = initClient()
 
-  val operator = "Operator"
-  val houseName = "CLEARCO"
+  private val operator = "Operator"
 
-  var operatorRole: Option[OperatorRole.Contract] = None
+  private var operatorRole: Option[OperatorRole.Contract] = None
   private val metadata = XsdMetadataReader.fromJSON(mkClasspathURL("classpath:acord/metadata/TXLifeJ/TXLife.json"))
 
   private val logger: Logger = LoggerFactory.getLogger("Commands")
@@ -45,25 +43,14 @@ object Commands {
     logger.info("Got operator role contract: " + operatorRole.get.id)
   }
 
-  // Flows
   def send(example: URL): Unit = {
     val decoder = new RequestClearingDecoder(OperatorRole.TEMPLATE_ID, operatorRole.get.id.contractId, metadata)
-    // val encoder = new FpMLEncoder("clearingAcknowledgement", metadata, schema)
-
     val cmd = decoder.decode("example", ByteStreams.toByteArray(example.openStream()))
     client.sendCommands("Acord - SubmitTXLife", operator, List(cmd))
-  }
-
-  def setTime(timeString: String): Unit = {
-    val time = Instant.parse(timeString)
-    client.setTime(time)
   }
 
   // TODO why does installing a protocol handler via sysprop not work?
   def mkClasspathURL(path: String): URL =
     new URL(null, path, new Handler())
-
-  def fetchUrl(url: URL): String =
-    new String(ByteStreams.toByteArray(url.openStream))
 
 }
